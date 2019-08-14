027c00b0da6fc600f3bafafef592c6c4f8cc563f
Nechaev363 <nechaevfmg@mail.ru>
2019-08-14 17:55:02 +0500

    over

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,52 +1,73 @@
 const calc = () => {
+
     const formCalk = document.querySelector('#card_order'),
         totalPrice = document.getElementById('price-total'),
         radioButton = formCalk.querySelectorAll('input[name="club-name"]'),
         inputMonth = formCalk.querySelectorAll('.time input'),
-        promocode = formCalk.querySelector('.price input');
+        promoCod = formCalk.querySelector('.price input');
     let priceMonth,
         radioButtonActive;
 
     const selectClub = () => {
+
         radioButton.forEach((e) => {
+
             if (e.checked) {
                 radioButtonActive = e.id;
             }
         });
     };
 
+
+
     const parsingPrice = () => {
+
         pricingMonth();
 
+
+
         inputMonth.forEach((e) => {
+
             e.setAttribute('data-price', priceMonth[e.id]);
+
         });
 
-        if(inputMonth.length > 0){
+
+
+        if (inputMonth.length > 0) {
+
             inputMonth[0].click();
+
         }
+
     };
+    const saleS = () => {
 
-    const sale = () => {
-        let totalSale = totalPrice.textContent;
-        if(promocode.value === 'ТЕЛО2019') {
-            let sale = totalSale * 30 / 100;
-            totalSale = totalSale - sale;
+        let totalP = totalPrice.textContent;
+        if (promoCod.value === 'ÒÅËÎ2019') {
 
-            totalSale = Math.ceil(totalSale);
+            let sale = totalP * 30 / 100;
+
+            totalP = totalP - sale;
+
+
+
+            totalP = Math.ceil(totalP);
+
         }
-        setTotalPrice(totalSale);
+
+        setTotalPrice(totalP);
+
     }
-    
 
     const setTotalPrice = (total) => {
+
         totalPrice.textContent = total;
-        promocode.value = '';
+
     };
 
     const pricingMonth = () => {
         selectClub();
-
         if (radioButtonActive === 'card_leto_mozaika') {
             priceMonth = {
                 m1: 1999,
@@ -63,23 +84,35 @@ const calc = () => {
             };
         }
     };
-    
+
     parsingPrice();
 
-    promocode.addEventListener('change', () => {
-        sale()
-    });
-    
+    promoCod.addEventListener('input', () => {
+
+        saleS();
+
+    })
+
     radioButton.forEach((e) => {
+
         e.addEventListener('click', () => {
+
+            promoCod.value = '';
+
             parsingPrice();
+
         });
+
     });
 
     inputMonth.forEach((e) => {
+
         e.addEventListener('click', () => {
+            promoCod.value = '';
             setTotalPrice(e.getAttribute('data-price'));
+
         });
     });
+
 };
 export default calc;
