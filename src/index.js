1e4c38c20ba293d242e36531e661c1d24476e178
Nechaev363 <nechaevfmg@mail.ru>
2019-08-14 12:54:47 +0500

    calculator

--- src/index.js
+++ src/index.js
@@ -430,3 +430,73 @@ const serviceSlide = () => {
 
 }
 serviceSlide();
+
+//calculator
+
+const calc = () => {
+    const formCalk = document.querySelector('#card_order'),
+        totalPrice = document.getElementById('price-total'),
+        radioButton = formCalk.querySelectorAll('input[name="club-name"]'),
+        inputMonth = formCalk.querySelectorAll('.time input');
+    let priceMonth,
+        radioButtonActive;
+
+    const selectClub = () => {
+        radioButton.forEach((e) => {
+            if (e.checked) {
+                radioButtonActive = e.id;
+            }
+        });
+    };
+
+    const parsingPrice = () => {
+        pricingMonth();
+
+        inputMonth.forEach((e) => {
+            e.setAttribute('data-price', priceMonth[e.id]);
+        });
+
+        if(inputMonth.length > 0){
+            inputMonth[0].click();
+        }
+    };
+
+    const setTotalPrice = (total) => {
+        totalPrice.textContent = total;
+    };
+
+    const pricingMonth = () => {
+        selectClub();
+
+        if (radioButtonActive === 'card_leto_mozaika') {
+            priceMonth = {
+                m1: 1999,
+                m2: 9900,
+                m3: 13900,
+                m4: 19900
+            };
+        } else if (radioButtonActive === 'card_leto_schelkovo') {
+            priceMonth = {
+                m1: 2999,
+                m2: 14900,
+                m3: 21900,
+                m4: 24900
+            };
+        }
+    };
+    
+    parsingPrice();
+    
+    radioButton.forEach((e) => {
+        e.addEventListener('click', () => {
+            parsingPrice();
+        });
+    });
+
+    inputMonth.forEach((e) => {
+        e.addEventListener('click', () => {
+            setTotalPrice(e.getAttribute('data-price'));
+        });
+    });
+};
+calc();
